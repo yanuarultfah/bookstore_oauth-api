8d38f399e5df7f2db432a98996d1c53aa4932c93
yanuarultfah <eragon.shapira10@gmail.com>
2024-11-10 22:31:05 +0700

    edit : change test with assert

--- src/domain/access_token/access_token_test.go
+++ src/domain/access_token/access_token_test.go
@@ -3,31 +3,44 @@ package accesstoken
 import (
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
+func TestAccessTokenConstant(t *testing.T) {
+	// if expirationTime != 24 {
+	// 	t.Error("expiration time should be 24 hours")
+	// }
+	assert.EqualValues(t, 24, expirationTime)
+}
 func TestGetNewAccessToken(t *testing.T) {
 	at := GetNewAccessToken()
-	if at.IsExpired() {
-		t.Error(" brand new access token should not be expired")
-	}
+	assert.False(t, at.IsExpired(), " brand new access token should not be expired")
+	// if at.IsExpired() {
+	// 	t.Error(" brand new access token should not be expired")
+	// }
 
-	if at.AccessToken != "" {
-		t.Error("new access token should not have defined access token id")
-	}
+	assert.EqualValues(t, "", at.AccessToken, "new access token should not have defined access token id")
+	// if at.AccessToken != "" {
+	// 	t.Error("new access token should not have defined access token id")
+	// }
 
-	if at.UserId != 0 {
-		t.Error("new access token should not have associate user id")
-	}
+	assert.True(t, at.UserId == 0, "new access token should not have associate user id")
+	// if at.UserId != 0 {
+	// 	t.Error("new access token should not have associate user id")
+	// }
 }
 
 func TestAccessTokenIsExpired(t *testing.T) {
 	at := AccessToken{}
+	assert.True(t, at.IsExpired(), "empty access token should be expired by default")
 
-	if !at.IsExpired() {
-		t.Error("Empty access token should be expired by default")
-	}
+	// if !at.IsExpired() {
+	// 	t.Error("Empty access token should be expired by default")
+	// }
 
 	at.Expires = time.Now().UTC().Add(3 * time.Hour).Unix()
+	assert.False(t, at.IsExpired(), "access token create three hours from now shoult not be expired by default")
 
 	if at.IsExpired() {
 		t.Error("access token create three hours from now shoult not be expired by default")
